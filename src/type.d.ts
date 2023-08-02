70257da4ee0a5f1e440f57da903dd3adb894a5c6
Leviakc <gomen98.jg@gmail.com>
2023-08-02 16:39:59 -0600

    changed types

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,27 +1,21 @@
-import { ACTION_TYPES } from "./consts";
-
 export interface Todo {
   id: string;
   title: string;
   completed: boolean;
 }
 
-export interface Todos {
-  Todos: Todo[];
-}
+export type Todos = Todo[];
 
 // export type TodoId = Pick<Todo, "id">;
 // export type TodoCompleted = Pick<Todo, "completed">;
-export type TodoId = Todo.id;
-export type TodoCompleted = Todo.completed;
+export type TodoId = Todo["id"];
+export type TodoCompleted = Todo["completed"];
 
 export type ActionOptions =
-  | { type: ACTION_TYPES.ADD; payload: Todo }
-  | { type: ACTION_TYPES.REMOVE; payload: Todo.id }
-  | { type: ACTION_TYPES.TOGGLE; payload: { id: string; completed: boolean } }
-  | { type: ACTION_TYPES.RE_ORDER_TODOS; payload: Todo[] }
-  | { type: ACTION_TYPES.REMOVE_COMPLETED; payload?: undefined };
-// TODO: investigate this issue
-// | { type: ACTION_TYPES.REMOVE_COMPLETED; payload?: undefined };
+  | { type: "ADD"; payload: { todo: Todo } }
+  | { type: "REMOVE"; payload: { id: TodoId } }
+  | { type: "TOGGLE"; payload: { id: TodoId; completed: TodoCompleted } }
+  | { type: "REORDER_TODOS"; payload: { todos: Todos } }
+  | { type: "REMOVE_COMPLETED" };
 
 export type FilterValue = "all" | "active" | "completed";
